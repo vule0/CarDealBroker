608fdabe69f8195ce67219034d6a90de827498fe
vule0 <vule2003@yahoo.com>
2025-03-19 15:04:59 -0400

    Update s3_utils.py

--- backend/s3_utils.py
+++ backend/s3_utils.py
@@ -11,7 +11,7 @@ load_dotenv()
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")  # Default to us-east-1
+AWS_REGION = os.getenv("AWS_REGION", "us-east-2")  # Default to us-east-1
 
 # Initialize S3 client
 def get_s3_client():
